034b5ad11a857084602fbe92a83a7e41c82a2e82
Denny Raymond <rrettob@gmail.com>
2020-04-18 15:17:44 +0800

    Finish convert html homepage to vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderGadiel />
+    <HeroCarousel />
+    <BannerGadiel />
+    <InstagramGadiel />
+    <PartnerlogoGadiel />
+    <FooterGadiel />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderGadiel from "@/components/HeaderGadiel.vue";
+import HeroCarousel from "@/components/HeroCarousel.vue";
+import BannerGadiel from "@/components/BannerGadiel.vue";
+import InstagramGadiel from "@/components/InstagramGadiel.vue";
+import PartnerlogoGadiel from "@/components/PartnerlogoGadiel.vue";
+import FooterGadiel from "@/components/FooterGadiel.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderGadiel,
+    HeroCarousel,
+    BannerGadiel,
+    InstagramGadiel,
+    PartnerlogoGadiel,
+    FooterGadiel,
+  },
+};
+</script>
